49f7459ce8673dc09b88864e2ac04282d9d3b760
carlos <carlos.teixeira56@aluno.ce.gov.br>
2024-11-07 16:49:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,5 +32,21 @@ while(continuar) {
         let baseR = prompt ('qual a base do retângulo?')
         let alturaR = prompt('qual a altura do retangulo?')
          alert('a area do retangulo é' + retan(baseR,alturaR))
-    } else (menu == 3 )
+    } else if(menu == '3' ) {
+        let lado = prompt('qual o lado do triangulo')
+        alert('a area do quadrado é ' + quadra(lado,lado))
+    } else if (menu == '4') {
+        let baseMaior = prompt('qual a base menor?')
+        let baseMenor = prompt ('qual o lsdo da base menor?')
+        let altura = prompt ('qual a altura?')
+        alert('a area do trapezio é' + trape(baseMaior,baseMenor,altura))
+    }else if (menu == '5') {
+        let raio = prommpt ('qual o raio do circulo?')
+        alert('a area o circulo é?' + circ(raio))
+    } else if (menu == '6') {
+        alert('saindo...')
+        continuar=false
+    } else [
+        alert('opcao invalida!')
+    ]
 }
